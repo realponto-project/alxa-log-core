ebe615b37cc21451ac155d64f09fcc2f6e46503e
jessiLeandro <jessi_leandro@hotmail.com>
2021-07-22 14:33:17 -0300

    fix: config database file

--- config/database.js
+++ config/database.js
@@ -1,7 +1,3 @@
-require('dotenv').config({
-  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
-})
-
 const DB_USERNAME = process.env.DB_USERNAME || 'postgres'
 const DB_HOST = process.env.DB_HOST || 'localhost'
 const DB_NAME = process.env.DB_NAME || 'jsl-postgres'
