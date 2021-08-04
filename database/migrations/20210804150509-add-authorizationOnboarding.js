14ff4a633673207eae1d5f21e3f1779c8631b42c
Guilherme Stain <guilhermestain@localhost.localdomain>
2021-08-04 14:49:10 -0300

    add: add flag authorizationOnboarding default false

--- database/migrations/20210804150509-add-authorizationOnboarding.js
+++ database/migrations/20210804150509-add-authorizationOnboarding.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('driver', 'authorizationOnboarding', {
+    await queryInterface.addColumn('drivers', 'authorizationOnboarding', {
       type: Sequelize.BOOLEAN,
       allowNull: false,
       defaultValue: false,
@@ -10,6 +10,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('driver', 'authorizationOnboarding')
+    await queryInterface.removeColumn('drivers', 'authorizationOnboarding')
   }
 };
